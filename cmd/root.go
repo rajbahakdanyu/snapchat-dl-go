c83b4fd7bbc622243ef6c115f2d6364746077589
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-09-25 13:00:09 +1000

    moved commands to a seperate file

--- cmd/root.go
+++ cmd/root.go
@@ -3,29 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
-
-	"github.com/spf13/cobra"
 )
-
-var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello World")
-	},
-}
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Snapchat-dl v0.1.0")
-	},
-}
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
