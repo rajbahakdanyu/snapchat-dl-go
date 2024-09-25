77f734cb0098a73065a3f88ac9def7839d587da7
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-09-25 12:58:07 +1000

    add version command

--- cmd/root.go
+++ cmd/root.go
@@ -14,8 +14,21 @@ var rootCmd = &cobra.Command{
 				  love by spf13 and friends in Go.
 				  Complete documentation is available at http://hugo.spf13.com`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		fmt.Println("Hello World")
 	},
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Hugo",
+	Long:  `All software has versions. This is Hugo's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Snapchat-dl v0.1.0")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
 }
 
 func Execute() {
