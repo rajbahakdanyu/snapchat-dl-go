dda9730795b2be7f6b9f00f98a565195f62f1ceb
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-09-25 13:43:19 +1000

    changes

--- cmd/commands.go
+++ cmd/commands.go
@@ -6,12 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const version = "0.1.0"
+
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "snapchat-dl",
+	Short: "Snapchat-dl is a tool to download Snapchat videos",
+	Long:  "Snapchat-dl is a tool to download Snapchat videos",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello World")
 	},
@@ -19,9 +19,9 @@ var rootCmd = &cobra.Command{
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
+	Short: "Print the version number of Snapchat-dl",
+	Long:  `All software has versions. This is Snapchat-dl's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Snapchat-dl v0.1.0")
+		fmt.Println("Snapchat-dl v" + version)
 	},
 }
